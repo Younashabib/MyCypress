34ac58e73cf7dd7d6013b28ea0cbebc5847e150d
YHK <younas.habib@gmail.com>
2023-09-05 13:31:51 +0200

    Adding Changes to Page Object

--- cypress/e2e/4-Web-Driver-Uni/contact-us-PageObject.js
+++ cypress/e2e/4-Web-Driver-Uni/contact-us-PageObject.js
@@ -1,4 +1,3 @@
-import Homepage_PO from "../../support/pageObjects/webdriver-uni/Homepage_PO";
 /// <reference types="cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
@@ -10,41 +9,36 @@ describe("Test Contact Us form via WebdriverUni", () => {
   });
 
   beforeEach(function () {
-    //cy.visit(Cypress.env("webdriveruni_homepage") + "/Contact-Us/contactus.html")
-    //cy.visit("http://webdriveruniversity.com/")
-    //cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })
-    const homepage_PO = new Homepage_PO();
-    homepage_PO.visitHompage();
-    homepage_PO.clickOn_ContactUs_Button();
+    cy.visit("http://www.webdriveruniversity.com");
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
   });
-
-  it("Should be able to submit a successful submission via contact us form", () => {
-    cy.document().should("have.property", "charset").and("eq", "UTF-8");
-    cy.title().should("include", "WebDriver | Contact Us");
-    cy.url().should("include", "contactus");
-    //cy.get('#contact-us').click({force: true})
-    // cy.get('[name="first_name"]').type(data.first_name);
-    // cy.get('[name="last_name"]').type(data.last_name);
-    // cy.get('[name="email"]').type(data.email)
-    // cy.get('textarea.feedback-input').type("How can I learn Cypress?")
-    // cy.get('[type="submit"]').click();
-    // cy.get('h1').should('have.text', 'Thank You for your Message!')
+  it.only("Should be able to submit a successful submission via contact us form", () => {
+    cy.webpage_WebDriverUniversity();
     cy.webdriverUni_ContactForm_Submission(
-      Cypress.env("first_name"),
+      data.first_name,
       data.last_name,
       data.email,
       "How can I learn Cypress?",
       "h1",
       "Thank You for your Message!"
+      //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+      //cy.visit("http://www.webdriveruniversity.com");
+      //cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
+      //cy.document().should("have.property", "charset").and("eq", "UTF-8");
+      //cy.title().should("include", "WebDriver | Contact Us");
+      //cy.url().should("include", "contactus");
+      //cy.get('#contact-us').click({force: true})
+      // cy.get('[name="first_name"]').type(data.first_name);
+      // cy.get('[name="last_name"]').type(data.last_name);
+      // cy.get('[name="email"]').type(data.email)
+      // cy.get('textarea.feedback-input').type("How can I learn Cypress?")
+      // cy.get('[type="submit"]').click();
+      // cy.get('h1').should('have.text', 'Thank You for your Message!')
     );
   });
 
   it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-    // cy.get('[name="first_name"]').type(data.first_name);
-    // cy.get('[name="last_name"]').type(data.last_name);
-    // cy.get('textarea.feedback-input').type("How can I learn Cypress?")
-    // cy.get('[type="submit"]').click();
-    // cy.get('body').contains('Error: all fields are required');
+    cy.webpage_WebDriverUniversity();
     cy.webdriverUni_ContactForm_Submission(
       data.first_name,
       data.last_name,
@@ -53,5 +47,13 @@ describe("Test Contact Us form via WebdriverUni", () => {
       "body",
       "Error: Invalid email address"
     );
+    //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    //cy.visit("http://www.webdriveruniversity.com");
+    //cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
+    // cy.get('[name="first_name"]').type(data.first_name);
+    // cy.get('[name="last_name"]').type(data.last_name);
+    // cy.get('textarea.feedback-input').type("How can I learn Cypress?")
+    // cy.get('[type="submit"]').click();
+    // cy.get('body').contains('Error: all fields are required');
   });
 });
