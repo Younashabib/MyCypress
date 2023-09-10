a02469a5568907914431fe74d31ad13fa9f61e23
YHK <younas.habib@gmail.com>
2023-09-10 09:13:05 +0200

    Changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,27 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'pxeseg',
+  projectId: "pxeseg",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    excludeSpecPattern: "cypress/e2e/1-getting-started",
+    excludeSpecPattern: "cypress/e2e/2-advanced-examples",
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000,
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
+    video: false,
+    //baseUrl: "https://automationteststore.com/",
+    //baseUrl: "https://www.webdriveruniversity.com/",
+    env: {
+      first_name: "Sarah", //Making a Global Variable and used in contact-us (WebDriver Uni).
+      webDriverUni_WebPage: "https://www.webdriveruniversity.com/",
+      //AutomationStore_WebPage: "https://automationteststore.com/", //See automation Test Store > Variables and Cy-Commands >     cy.visit(Cypress.env("AutomationStore_WebPage") + "index.php?rt=content/contact");
     },
   },
 });
